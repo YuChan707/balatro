b59f990e3db89d418c8d67a97f208663bc98eb02
CYB101NAME <CYB101EMAIL>
2025-09-26 16:40:19 -0400

    update

---
+++ frontend/src/components/menu.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+export default function Menu() {
+    return (
+        <div>
+      <h1 className='title'>Menu</h1>
+      <div className='Button-row'>
+        <button>Set</button>
+      
+      
+        <button >competiton</button>
+        
+      </div>
+      <div className='upLoad'>
+        <button>Upload More </button>
+      </div>
+        
+ 
+           
+
+        </div>
+    );
+}

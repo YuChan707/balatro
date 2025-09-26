adccb15656ead0bf16f3d350c041cc437e3bcd0d
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-09-26 17:30:15 -0400

    add: card component

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,6 +1,20 @@
 import React from "react";
+import backCard from "./../assets/cardBack.png";
 
-function Card({}) {
+function Card({ side }) {
+  if (side === "back") {
+    return (
+      <div
+        className="w-[136px] h-[200px]"
+        style={{
+          backgroundImage: url(backCard),
+        }}
+      >
+        lol
+      </div>
+    );
+  }
+
   return (
     <div>
       <p className="font-bold text-2xl">Card</p>

a060551fa9812cf61c0f49d4c460b95b7535e7fb
CYB101NAME <CYB101EMAIL>
2025-09-26 20:08:23 -0400

    menunew

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import Card from "./components/Card";
 import Menu from "./components/menu";
+import Button from "./components/button";
 
 function App() {
   return (
     
     <>
      <Menu/>
-      
+      <Button text={'hello'}/>
     </>
   );
 }
